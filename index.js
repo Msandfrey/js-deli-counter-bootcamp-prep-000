7315260b9927acd30a21317b5f265a0839bb6c0d
Matthew Sandfrey <msandfrey@sbcglobal.net>
2017-10-04 22:36:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ function takeANumber(line, name){
 function nowServing(line){
   if(line.length === 0) return "There is nobody waiting to be served!"
   else{
-    return line.shift()
+    return `Currently serving ${line.shift()}.`
   }
 }
 function currentLine(line){
   if(line.length === 0) return "The line is currently empty."
   else{
-    var string = "The line is currently: "
+    var string = "The line is currently:"
     for(var i = 0; i < line.length; i++){
       var place = i + 1
       var person = line[i]
-      string += `${place}. ${person} `
+      string += ` ${place}. ${person}`
     }
   }
   return string
